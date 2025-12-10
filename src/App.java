5ae6ab7f7da59223e12d97099dccdb695735a384
KareemMohamed75 <km571218@gmail.com>
2025-12-10 14:22:18 +0200

    first version for ball and Handle method and draw game

---
+++ src/App.java
@@ -0,0 +1,12 @@
+
+public class App {
+  public static void main(String[] args) throws Exception {
+
+
+    // HighScores highScores = new HighScores();
+    // HowToPlay howToPlay = new HowToPlay();
+    new Game();
+    // Timer timer = new Timer();
+    // System.out.println("shit");
+  }
+}
