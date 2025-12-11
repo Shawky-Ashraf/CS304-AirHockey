fbe0e7343bbcfaa4b9042c53aa932e1a31197339
Mohab-mostafa <popmostafa59@gmail.com>
2025-12-11 03:33:13 +0200

    updates and levels

--- src/App.java
+++ src/App.java
@@ -3,8 +3,7 @@ public class App {
   public static void main(String[] args) throws Exception {
     // new HowToPlay();
     // new HighScores();
-    new Game();
-    // new Home();
-    // add line to test login
+//    new Game();
+     new Home();
   }
 }
